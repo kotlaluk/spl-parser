9f6262404e346d97bfa92ab52e4921236048b26d
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2020-01-29 14:56:00 +0100

    Implemented parsing of conf

--- spl_parser/searchbnf_parser.py
+++ spl_parser/searchbnf_parser.py
@@ -1,17 +1,39 @@
+import configparser
+import json
+
 from spl_parser.spl_objects import SPLTerm
 from spl_parser.spl_objects import SPLCommand
 
 
-def parse_json_term(json_term):
+def parse_json(json_file):
+    with open(json_file) as f:
+        json_data = json.loads(f.read())
+        spl_terms = dict()
+        for json_term in json_data["entry"]:
+            spl_term = parse_spl_term(json_term["name"], json_term["content"])
+            spl_terms[spl_term.name] = spl_term
+        return spl_terms
+
+
+def parse_conf(conf_file):
+    confparser = configparser.RawConfigParser(strict=False, allow_no_value=True)
+    confparser.read(conf_file)
+    spl_terms = dict()
+    for section in confparser.sections():
+        spl_term = parse_spl_term(section, confparser[section])
+        spl_terms[spl_term.name] = spl_term
+    return spl_terms
+
+
+def parse_spl_term(name, data):
     # TODO global try/except - logging
-    name = json_term["name"]
-    syntax = json_term["content"]["syntax"]
+    syntax = data["syntax"]
     if name.endswith("-command"):
         name = name.replace("-command", "")
 
         # Get aliases if present
         try:
-            aliases = json_term["content"]["alias"].split(",")
+            aliases = [x.strip() for x in data["alias"].split(",")]
         except KeyError:
             aliases = list()
 
@@ -22,17 +44,8 @@ def parse_json_term(json_term):
 
     # Optionally fetch description
     try:
-        description = json_term["content"]["description"]
+        description = data["description"]
         spl_term.description = description
     except KeyError:
         pass
     return spl_term
-
-
-def parse_json(json_data):
-    spl_terms = dict()
-    json_terms = json_data["entry"]
-    for json_term in json_terms:
-        spl_term = parse_json_term(json_term)
-        spl_terms[spl_term.name] = spl_term
-    return spl_terms
